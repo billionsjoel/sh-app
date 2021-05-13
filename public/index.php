01b8f507f2925a2c669694bca6423a9698399017
Billions Joel <joelatugonza@gmail.com>
2021-05-13 07:00:12 +0300

    bug fix

--- public/index.php
+++ public/index.php
@@ -47,8 +47,10 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 // set the public path to this directory
-$app->bind('path.public', function() {
+$app->bind('path.public', function () {
     return __DIR__;
+});
+
 
 $kernel = $app->make(Kernel::class);
 
