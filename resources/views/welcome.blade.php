85607c670c9a1f14ffb5046aa77d98ea5bf1cd96
Billions Joel <joelatugonza@gmail.com>
2021-06-22 07:20:00 +0300

    resizing images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1161,7 +1161,7 @@
                         <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
                             data-image="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
                             data-target="#image-gallery">
-                            <img class="img-thumbnail" src="{{url('images/bookcovers/book1.jpg')}}"
+                            <img class="img-thumbnail" src="{{url('images/bookcovers/book1.jpeg')}}"
                                 alt="Another alt text">
                         </a>
                     </div>
@@ -1169,7 +1169,7 @@
                         <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
                             data-image="https://images.pexels.com/photos/158971/pexels-photo-158971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
                             data-target="#image-gallery">
-                            <img class="img-thumbnail" src="{{url('images/bookcovers/book2.jpg')}}"
+                            <img class="img-thumbnail" src="{{url('images/bookcovers/book2.jpeg')}}"
                                 alt="Another alt text">
                         </a>
                     </div>
@@ -1178,7 +1178,7 @@
                         <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
                             data-image="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
                             data-target="#image-gallery">
-                            <img class="img-thumbnail" src="{{url('images/bookcovers/book3.jpg')}}"
+                            <img class="img-thumbnail" src="{{url('images/bookcovers/book3.jpeg')}}"
                                 alt="Another alt text">
                         </a>
                     </div>
@@ -1186,7 +1186,7 @@
                         <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Test1"
                             data-image="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
                             data-target="#image-gallery">
-                            <img class="img-thumbnail" src="{{url('images/bookcovers/book4.jpg')}}"
+                            <img class="img-thumbnail" src="{{url('images/bookcovers/book4.jpeg')}}"
                                 alt="Another alt text">
                         </a>
                     </div>
@@ -1194,7 +1194,7 @@
                         <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
                             data-image="https://images.pexels.com/photos/158971/pexels-photo-158971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
                             data-target="#image-gallery">
-                            <img class="img-thumbnail" src="{{url('images/bookcovers/book5.jpg')}}"
+                            <img class="img-thumbnail" src="{{url('images/bookcovers/book5.jpeg')}}"
                                 alt="Another alt text">
                         </a>
                     </div>
