bc12c01133c72e605ed44730660d628db98a194e
Billions Joel <joelatugonza@gmail.com>
2021-07-07 23:23:40 +0300

    adding Dashboard UI

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,17 +2,26 @@
 <div class="sidebar-heading">ScribeHouse</div>
 <hr>
 <div class="list-group list-group-flush">
-    <a class="list-group-item list-group-item-action bg-light" href="{{url('home')}}">Dashboard</a>
-    <a class="list-group-item list-group-item-action bg-light mt-4" href="{{ url('create-article') }}">Blog</a>
-    <a class="list-group-item list-group-item-action bg-light" href="{{ url('view-subscriptions') }}">Email List</a>
-    <a class="list-group-item list-group-item-action bg-light" href="{{ url('create-book-cover') }}">Book Cover</a>
-    <a class="list-group-item list-group-item-action bg-light" href="{{ url('create-testimonial') }}">Testimonial</a>
-    <a class="list-group-item list-group-item-action bg-light" href="{{ url('view-messages') }}">Messages</a>
-    <a class="list-group-item list-group-item-action bg-light" href="#!">Add User Account</a>
+    <a class="list-group-item list-group-item-action bg-light" href="{{url('home')}}"><i
+            class="fa fa-tachometer-alt"></i> &nbsp; Dashboard</a>
+    <a class="list-group-item list-group-item-action bg-light" href="{{ url('create-article') }}"><i
+            class="fa fa-blog"></i> &nbsp; Blog</a>
+    <a class="list-group-item list-group-item-action bg-light" href="{{ url('view-subscriptions') }}"><i
+            class="fa fa-mail-bulk"></i> &nbsp;Email List</a>
+    <a class="list-group-item list-group-item-action bg-light" href="{{ url('create-book-cover') }}"><i
+            class="fa fa-box-open"></i> &nbsp;Book Cover</a>
+    <a class="list-group-item list-group-item-action bg-light" href="{{ url('create-testimonial') }}"><i
+            class="fa fa-bullhorn"></i> &nbsp;Testimonial</a>
+    <a class="list-group-item list-group-item-action bg-light" href="{{ url('view-messages') }}"><i
+            class="fa fa-envelope"></i> &nbsp;Messages</a>
+    <a class="list-group-item list-group-item-action bg-light" href="#!"><i class="fa fa-user"></i> &nbsp; Add User
+        Account
+    </a>
     <li class="list-group-item list-group-item-action bg-light mt-4">
         <form id="logout-form" action="{{ route('logout') }}" method="POST">
             @csrf
-            <button class="dropdown-item" type="submit" href="#!">Log Out</button>
+            <button class="btn btn-warning btn-block" type="submit" href="#!"> Log
+                Out <i class="fa fa-power-off"></i></button>
         </form>
     </li>
 </div>
