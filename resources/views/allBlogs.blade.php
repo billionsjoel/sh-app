2b6d3943a9c315a2d1953a6ca8245323d4b709b7
Billions Joel <joelatugonza@gmail.com>
2021-09-09 09:53:04 +0300

    character trim

--- resources/views/allBlogs.blade.php
+++ resources/views/allBlogs.blade.php
@@ -87,7 +87,7 @@
         <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
             <div class="col-md-6 px-0">
                 <h1 class="display-4 fst-italic">{{ $featured->title }}</h1>
-                <p class="lead my-3">{!! \Illuminate\Support\Str::limit($featured->body, 1000) !!}</p>
+                <p class="lead my-3">{!! \Illuminate\Support\Str::limit($featured->body, 500) !!}</p>
                 <p class="lead mb-0"><a href="{{ url('/blogs?id=' . $featured->id) }}"
                         class="text-white fw-bold">Continue reading...</a></p>
             </div>
