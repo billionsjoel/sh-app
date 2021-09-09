6b551c632081c7ea03af48e2a2d6ee929895a45b
Billions Joel <joelatugonza@gmail.com>
2021-09-09 21:29:24 +0300

    title

--- resources/views/allBlogs.blade.php
+++ resources/views/allBlogs.blade.php
@@ -86,8 +86,8 @@
 
         <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
             <div class="col-md-6 px-0">
-                <h1 class="display-4 fst-italic">{{ $featured->title }}</h1>
-                <p class="lead my-3">{!! \Illuminate\Support\Str::limit($featured->body, 500) !!}</p>
+                <h1 class="display-4 fst-italic">{!! \Illuminate\Support\Str::limit($featured->title, 20) !!}</h1>
+                <p class="lead my-3">{!! \Illuminate\Support\Str::limit($featured->body, 800) !!}</p>
                 <p class="lead mb-0"><a href="{{ url('/blogs?id=' . $featured->id) }}"
                         class="text-white fw-bold">Continue reading...</a></p>
             </div>
