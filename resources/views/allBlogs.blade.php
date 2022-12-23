c7fdb010ab5191dc7228d14aac3ce3eda7298e1d
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-12-23 18:56:05 +0300

    Fix comments and footer

--- resources/views/allBlogs.blade.php
+++ resources/views/allBlogs.blade.php
@@ -37,7 +37,6 @@
                 font-size: 3.5rem;
             }
         }
-
     </style>
 
 
@@ -66,9 +65,9 @@
                 </div>
                 <div class="col-4 d-flex justify-content-end align-items-center">
                     <a class="link-secondary" href="#" aria-label="Search">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3"
-                            role="img" viewBox="0 0 24 24">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"
+                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            class="mx-3" role="img" viewBox="0 0 24 24">
                             <title>Search</title>
                             <circle cx="10.5" cy="10.5" r="7.5" />
                             <path d="M21 21l-5.2-5.2" />
@@ -124,8 +123,8 @@
                             role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice"
                             focusable="false">
                             <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                                dy=".3em">Thumbnail</text>
+                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%"
+                                y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
                         </svg>
 
                     </div>
@@ -143,12 +142,12 @@
                             reading</a>
                     </div>
                     <div class="col-auto d-none d-lg-block">
-                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg"
-                            role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice"
-                            focusable="false">
+                        <svg class="bd-placeholder-img" width="200" height="250"
+                            xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+                            preserveAspectRatio="xMidYMid slice" focusable="false">
                             <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                                dy=".3em">Thumbnail</text>
+                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%"
+                                y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
                         </svg>
 
                     </div>
@@ -174,13 +173,13 @@
                         <hr>
                     @empty
                         <p>There are currently no blogs at the moment. come back later</p>
-
                     @endforelse
                 </article>
 
                 <nav class="blog-pagination" aria-label="Pagination">
                     <a class="btn btn-outline-primary" href="#">Older</a>
-                    <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+                    <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1"
+                        aria-disabled="true">Newer</a>
                 </nav>
 
             </div>
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -68,7 +68,6 @@
         *::after {
             box-sizing: border-box;
         }
-
     </style>
     <style>
         .navbar-fixed-top.scrolled {
@@ -591,7 +590,6 @@
             background: #FA6742;
             border-color: #FA6742;
         }
-
     </style>
     <style>
         .flash-message {
@@ -600,7 +598,6 @@
             right: 20px;
             z-index: 100;
         }
-
     </style>
 
 </head>
@@ -608,17 +605,16 @@
 <body>
 
     @if ($flash = session('message'))
-
         <div id="flash-message" class="alert alert-success flash-message mt-4" role="alert">
 
             {{ $flash }}
 
         </div>
-
     @endif
     <!-- Navigation -->
     <section>
-        <nav class="navbar navbar-expand-md navbar-dark navbar-fixed-top scrolled fixed-top shadow-lg small" id="header">
+        <nav class="navbar navbar-expand-md navbar-dark navbar-fixed-top scrolled fixed-top shadow-lg small"
+            id="header">
             <img src="{{ asset('/images/scribelogo.svg') }}" alt="" width="5%" class="ml-4">
             <a class="navbar-brand ml-4" href="{{ url('/') }}">Scribe House</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04"
@@ -664,7 +660,6 @@
     <div class="container" style="margin-top: 8%;">
 
         @forelse($blogs as $blog)
-
             <div class="row mt-4">
 
                 <!-- Post Content Column -->
@@ -834,7 +829,10 @@
                 </div>
             </div>
         @empty
-            <p>There are no comments on this blog at the moment. Be the first to comment</p>
+            <div class="mb-4 pb-4">
+                <p class="mb-4 alert alert-primary">There are no comments on this blog at the moment. Be the first to
+                    comment</p>
+            </div>
         @endforelse
 
 
@@ -943,201 +941,7 @@
     <!-- /.container -->
 
     <!-- Footer -->
-    <footer id="dk-footer" class="dk-footer text-white mt-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 col-lg-4">
-                    <div class="dk-footer-box-info">
-                        <a href="index.html" class="footer-logo">
-                            <img src="{{ asset('images/scribelogo.svg') }}" alt="footer_logo"
-                                class="___class_+?81___" height="100px">
-                        </a>
-                        <p class="footer-info-text">
-                            Scribe House recognises the scarcity of excellent, affordable editors in Uganda and
-                            Africa,
-                            and
-                            thus
-                            offers unique and specialised editing services.
-                        </p>
-                        <div class="footer-social-link">
-                            <h3>Follow us</h3>
-                            <ul>
-                                <li>
-                                    <a href="{{ url('https://www.facebook.com/scribehouse') }}">
-                                        <i class="fa fa-facebook"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ url('https://twitter.com/scribehouse') }}">
-                                        <i class="fa fa-twitter"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ url('https://www.instagram.com/scribehouse') }}">
-                                        <i class="fa fa-instagram"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- End Social link -->
-                    </div>
-                    <!-- End Footer info -->
-                    <div class="footer-awarad">
-                        <img src="images/icon/best.png" alt="">
-                        <p>ScribeHouse 2020</p>
-                    </div>
-                </div>
-                <!-- End Col -->
-                <div class="col-md-12 col-lg-8">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="contact-us">
-                                <div class="contact-icon">
-                                    <i class="fa fa-map-o" aria-hidden="true"></i>
-                                </div>
-                                <!-- End contact Icon -->
-                                <div class="contact-info">
-                                    {{-- <h2>Kiira</h2> --}}
-                                    <h5>Kampala, Uganda.</h5>
-                                    <p> scribehouse.ug@gmail.com.</p>
-                                </div>
-                                <!-- End Contact Info -->
-                            </div>
-                            <!-- End Contact Us -->
-                        </div>
-                        <!-- End Col -->
-                        <div class="col-md-6">
-                            <div class="contact-us contact-us-last">
-                                <div class="contact-icon">
-                                    <i class="fa fa-volume-control-phone" aria-hidden="true"></i>
-                                </div>
-                                <!-- End contact Icon -->
-                                <div class="contact-info">
-                                    <h3>(+256)782-682-678.</h3>
-                                    {{-- <h3>(+256)-702-152894</h3> --}}
-                                    <p>Give us a call.</p>
-                                </div>
-                                <!-- End Contact Info -->
-                            </div>
-                            <!-- End Contact Us -->
-                        </div>
-                        <!-- End Col -->
-                    </div>
-                    <!-- End Contact Row -->
-                    <div class="row">
-                        <div class="col-md-12 col-lg-6">
-                            <div class="footer-widget footer-left-widget">
-                                <div class="section-heading">
-                                    <h3>Useful Links</h3>
-                                    <span class="animate-border-- border-black"></span>
-                                </div>
-                                <ul>
-                                    <li>
-                                        <a href="{{ url('/#about') }}">About Us</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/#services') }}">Services</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/#gallery') }}">Projects</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/#team') }}">Our Team</a>
-                                    </li>
-                                </ul>
-                                <ul>
-                                    <li>
-                                        <a href="{{ url('/#contact') }}">Contact Us</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/#gallery') }}">Gallery</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/#clients') }}">Clients</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ '/#why' }}">Why Scribe House</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <!-- End Footer Widget -->
-                        </div>
-                        <!-- End col -->
-                        <div class="col-md-12 col-lg-6">
-                            <div class="footer-widget">
-                                <div class="section-heading">
-                                    <h3>Subscribe</h3>
-                                    <span class="animate-border-- border-black"></span>
-                                </div>
-                                <p>
-                                    <!-- Don’t miss to subscribe to our new feeds, kindly fill the form below. -->
-                                    Subscribe to our news-letter to receive more updates and offers.
-                                </p>
-                                <form action="#">
-                                    <div class="form-row">
-                                        <div class="col dk-footer-form">
-                                            <input type="email" class="form-control" placeholder="Email Address">
-                                            <button type="submit">
-                                                <i class="fa fa-send"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                                <!-- End form -->
-                            </div>
-                            <!-- End footer widget -->
-                        </div>
-                        <!-- End Col -->
-                    </div>
-                    <!-- End Row -->
-                </div>
-                <!-- End Col -->
-            </div>
-            <!-- End Widget Row -->
-        </div>
-        <!-- End Contact Container -->
-
-
-        <div class="copyright">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <span>Copyright © 2020, All Right Reserved.</span>
-                    </div>
-                    <!-- End Col -->
-                    <div class="col-md-6">
-                        <div class="copyright-menu">
-                            <ul>
-                                <li>
-                                    <a href="#">Home</a>
-                                </li>
-                                <li>
-                                    <a href="#">Terms</a>
-                                </li>
-                                <li>
-                                    <a href="#">Privacy Policy</a>
-                                </li>
-                                <li>
-                                    <a href="#">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- End col -->
-                </div>
-                <!-- End Row -->
-            </div>
-            <!-- End Copyright Container -->
-        </div>
-        <!-- End Copyright -->
-        <!-- Back to top -->
-        <div id="back-to-top" class="back-to-top">
-            <button class="btn btn-dark" title="Back to Top" style="display: block;">
-                <i class="fa fa-angle-up"></i>
-            </button>
-        </div>
-        <!-- End Back to top -->
-    </footer>
+    @include('layouts.footer')
     <script>
         setTimeout(function() {
             $('#flash-message').fadeOut('slow');
