85b085941c89ab0ee3c4a04b8568091acbb5ac5b
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2023-02-16 18:35:34 +0300

    Update home.blade.php
    Add panels for data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,42 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<h1 class="mt-4">Welcome to Scribehouse.</h1>
+    <p class="mt-4 lead container">Welcome to Scribehouse.</p>
 
+    <div class="row container mx-auto mt-4">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Messages
+                </div>
+                <div class="card-body">
+                    <p class="card-text display-6 text-center">9</p>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">view</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Book Covers
+                </div>
+                <div class="card-body">
+                    <p class="card-text display-6 text-center">13</p>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">view</a>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Articles
+                </div>
+                <div class="card-body">
+                    <p class="card-text display-6 text-center">8</p>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">view</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
