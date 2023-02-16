dc8bd09930fc11d1d276355b5da53bf83f503c19
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2023-02-16 15:48:15 +0300

    Move book section

---
+++ resources/views/components/book-section.blade.php
@@ -0,0 +1,116 @@
+    <section class="books" id="covers">
+        <div class="content ">
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="left">
+                        <div class="l-content">
+                            <div class="heading display-3">In the beginning was the <span class="blue"> word ...
+                                </span>
+                            </div>
+                            {{-- <div class="lb-paragraph">Start with a goal, get a plan, and set targets for that goal.
+                                SCRIBE HOUSE
+                                focuses
+                                on
+                                helping you achieve your desired goal.</div> --}}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="right">
+                        <div class="r-content">
+                            <div class="b-icon"><a href="{{ url('/covers') }}" class="text-decoration-none"><i
+                                        class="fas fa-caret-right"></i></a></div>
+                            <div class="title">
+                                <h5> <a href="{{ url('/covers') }}" class="text-black text-decoration-none">OUR
+                                        WORKS</a> </h5>
+                            </div>
+                            <div class="image">
+                                <a href="{{ url('/covers') }}" class="text-decoration-none"><img class="b-image"
+                                        src="{{ asset('images/bookcovers/book3.jpeg') }}" alt="">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 mt-4">
+                <div class="wrapper">
+                    <div class="outer">
+                        <img class="b-img shadow" src="{{ asset('images/bookcovers/bishop-luke-orombi.jpg') }}"
+                            alt="Bishop Luke Orombi">
+                        <div class="chev"><i class="fa fa-angle-right chev-color"></i></div>
+                    </div>
+                    <div class="inner">
+                        <div class="title mt-3">
+                            <h5 class="text-white">His Grace</h5>
+                        </div>
+                        <div class="description  text-center p-2">The autobiography of Bishop Henry Luke Orombi.</div>
+                        <a href="{{ url('covers') }}" class="btn btn-book shadow mb-3">&nbsp;&nbsp;Buy Book
+                            &nbsp;&nbsp;<i class="fa fa-angle-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-4">
+                <div class="wrapper">
+                    <div class="outer">
+                        <img class="b-img shadow" src="{{ asset('images/bookcovers/kiima.jpg') }}" alt="Say No To Fear">
+                        <div class="chev"><i class="fa fa-angle-right chev-color"></i></div>
+                    </div>
+                    <div class="inner">
+                        <div class="title mt-3">
+                            <h5 class="text-white">Say No to Fear</h5>
+                        </div>
+                        <div class="description text-center p-2">Learn to overcome five of our most common fears.</div>
+                        <a href="{{ url('covers') }}" class="btn btn-book shadow mb-3">&nbsp;&nbsp;Buy Book
+                            &nbsp;&nbsp;<i class="fa fa-angle-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-4">
+                <div class="wrapper">
+                    <div class="outer">
+                        <img class="b-img shadow" src="{{ asset('images/bookcovers/benjamin.png') }}"
+                            alt="Benjamin photo">
+                        <div class="chev"><i class="fa fa-angle-right chev-color"></i></div>
+                    </div>
+                    <div class="inner">
+                        <div class="title mt-3">
+                            <h5 class="text-white text-center">Emonevate</h5>
+                        </div>
+                        <div class="description text-center p-2">A poetry collection that will move you from an
+                            emotional rollercoaster to faith in God.</div>
+                        <a href="{{ url('covers') }}" class="btn btn-book shadow mb-3">&nbsp;&nbsp;Buy Book
+                            &nbsp;&nbsp;<i class="fa fa-angle-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-4">
+                <div class="wrapper">
+                    <div class="outer">
+                        <img class="b-img shadow mb-3" src="{{ asset('images/bookcovers/truck-driver.png') }}"
+                            alt="truck-driver">
+                        <div class="chev"><i class="fa fa-angle-right chev-color"></i></div>
+
+                    </div>
+                    <div class="inner">
+                        <div class="title mt-3">
+                            <h5 class="text-white">Truck Driver</h5>
+                        </div>
+                        <div class="description text-center p-2">A creative non-fictional story of the life of an East
+                            African truck driver.
+                        </div>
+                        <a href="{{ url('covers') }}" class="btn btn-book shadow mb-3">&nbsp;&nbsp;Buy Book
+                            &nbsp;&nbsp;<i class="fa fa-angle-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
