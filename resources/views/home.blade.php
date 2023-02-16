94d9f0adc00b10af7594e20e2bc3b3880cd76acf
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2023-02-16 18:40:39 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
                     Messages
                 </div>
                 <div class="card-body">
-                    <p class="card-text display-6 text-center">9</p>
+                    <p class="card-text display-6 text-center">60</p>
                     <a href="#" class="btn btn-primary btn-sm btn-block">view</a>
                 </div>
             </div>
@@ -34,6 +34,42 @@
                 </div>
                 <div class="card-body">
                     <p class="card-text display-6 text-center">8</p>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">view</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row container mx-auto mt-4 pt-4">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Email List
+                </div>
+                <div class="card-body">
+                    <p class="card-text display-6 text-center">7</p>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">view</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Testimonials
+                </div>
+                <div class="card-body">
+                    <p class="card-text display-6 text-center">0</p>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">view</a>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    User Accounts
+                </div>
+                <div class="card-body">
+                    <p class="card-text display-6 text-center">2</p>
                     <a href="#" class="btn btn-primary btn-sm btn-block">view</a>
                 </div>
             </div>
