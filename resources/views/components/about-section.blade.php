4e0d1aee1ae7c915b3017b0756ca643235c326ed
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2023-02-16 15:49:24 +0300

    Move about section

---
+++ resources/views/components/about-section.blade.php
@@ -0,0 +1,22 @@
+    <section class="about" id="about">
+        <h1 class="text-center s-title blue">About Us</h1>
+        <div class="ab-border light-blue"></div>
+        <div class="about-info container">
+            <p>We love books. We love books good books. We love words, and the different ways in which words can be
+                published. That’s
+                why our mission is to help you publish your content well.</p>
+
+            <p> Recognising the scarcity of excellent, affordable editors in Uganda and Africa, Scribe House provides
+                professional
+                editing, and book production services. We offer publishing and promotional advice to firms and
+                individuals across the
+                continent. Publishing consultancies and editorial services are comfortably transacted online, making it
+                easy for us to
+                serve you wherever you are.</p>
+
+            <p>We are a home for scribes.<br>
+                A scribe’s house.<br>
+                Scribe House.</p>
+            {{-- {!! $services->about ?? "" !!} --}}
+        </div>
+    </section>
