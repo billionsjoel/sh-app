ca754013c50125bff302e749171e3f634e7aef47
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2023-02-19 16:59:09 +0300

    Fix dynamic images on articles

--- resources/views/allBlogs.blade.php
+++ resources/views/allBlogs.blade.php
@@ -51,6 +51,12 @@
         .mg-top {
             margin-top: 6rem;
         }
+
+        .article-img {
+            width: 10rem;
+            height: 10rem;
+            object-fit: cover;
+        }
     </style>
 </head>
 
@@ -58,7 +64,7 @@
     @include('components.welcomePage.navbar')
 
     <main class="container mb-4">
-        <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark mg-top">
+        <div class="row p-md-5 mb-4 text-white rounded bg-dark mg-top">
             <div class="col-md-12 px-0">
                 <h1 class="display-4 fst-italic">{{ $featured->title }}</h1>
                 <p class="lead my-3">{!! \Illuminate\Support\Str::limit($featured->body, 500) !!}</p>
@@ -67,11 +73,11 @@
             </div>
         </div>
 
-        <div class="row mb-2">
+        <div class="row mb-2 d-flex justify-content-between">
             <div class="col-md-6">
                 <div
-                    class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static">
+                    class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative d-flex justify-content-center align-items-center">
+                    <div class="col p-4 flex-column position-static">
                         <strong class="d-inline-block mb-2 text-primary">Latest</strong>
                         <h3 class="mb-0">{{ $latest->title }}</h3>
                         <div class="mb-1 text-muted">{{ $latest->created_at }}</div>
@@ -79,37 +85,27 @@
                         <a href="{{ url('/blogs?id=' . $latest->id) }}" class="stretched-link">Continue reading</a>
                     </div>
                     <div class="col-auto d-none d-lg-block">
-                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg"
-                            role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice"
-                            focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%"
-                                fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
+                        <img class="article-img" src="{{ asset('images/articles/' . $latest->image) }}" alt=""
+                            height="100" width="100">
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-5">
                 <div
-                    class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static">
+                    class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative d-flex justify-content-center align-items-center">
+                    <div class="col p-3 d-flex flex-column position-static">
                         <strong class="d-inline-block mb-2 text-success">Promoted</strong>
                         <h3 class="mb-0">{{ $promoted->title }}</h3>
-                        <div class="mb-1 text-muted">{{ $promoted->created_at }}</div>
+                        <div class="text-muted">{{ $promoted->created_at }}</div>
                         <p class="mb-auto">{!! \Illuminate\Support\Str::limit($promoted->body, 100) !!}</p>
                         <a href="{{ url('/blogs?id=' . $promoted->id) }}" class="stretched-link">Continue
                             reading</a>
                     </div>
                     <div class="col-auto d-none d-lg-block">
-                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg"
-                            role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice"
-                            focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%"
-                                fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
+                        <div class="col-auto d-none d-lg-block">
+                            <img class="article-img" src="{{ asset('images/articles' . $promoted->image) }}"
+                                alt="" height="100" width="100">
+                        </div>
                     </div>
                 </div>
             </div>
